cf30101c6f5de3195121be554cad6328c488d2d7
1234abhibisht <abhibisht129@gmail.com>
2025-09-26 06:45:31 +0000

    factorialRecursion

---
+++ FindingFactorialOfaNumberUsingRecursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int factorial(int n) {
+    if(n == 0 || n == 1) {
+        return 1;
+    }
+    int x = n * factorial(n - 1);
+    return x;
+}
+int main() {
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    printf("Factorial of %d is %d",n,factorial(n));
+    return 0;
+}
